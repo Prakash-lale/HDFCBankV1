7205c78a3596b746eb887555886c93e0e02044b3
Prakash <prakashlaleabc@gmail.com>
2022-11-27 21:21:21 +0530

    Added code to handle alert box version 2

--- src/test/java/qa/HDFCBank/TestCases/TC_Login_01.java
+++ src/test/java/qa/HDFCBank/TestCases/TC_Login_01.java
@@ -11,37 +11,47 @@ import qa.HDFCBank.Pages.homePage;
 import qa.HDFCBank.Pages.loginPage;
 import qa.HDFCBank.Utils.readConfig;
 
+@Listeners({ qa.HDFCBank.Utils.Reporting.class })
+public class TC_Login_01 extends BaseClass {
 
-@Listeners({qa.HDFCBank.Utils.Reporting.class})
-public class TC_Login_01 extends BaseClass{
-	
 	loginPage lp;
 	homePage hp;
 	readConfig readconfig = new readConfig();
-	
+
 	public TC_Login_01() {
 		super();
 	}
-	
-	
-	//invocationCount is an attribute that defines the number of time a single method has to run in single execution
+
+	// invocationCount is an attribute that defines the number of time a single
+	// method has to run in single execution
 	@Test(invocationCount = 3)
 	public void loginTest() {
 		lp = new loginPage();
 		lp.login(readconfig.getUserId(), readconfig.getPwd());
-		driver.switchTo().alert().accept();
-		hp = new homePage();
-		System.out.println(hp.getmanagerId());
-		Assert.assertEquals(hp.getmanagerId(), "Manger Id : mngr452393");
+		if (isAlertPrasent() == true) {
+			driver.switchTo().alert().accept();
+		} else {
+			hp = new homePage();
+			System.out.println(hp.getmanagerId());
+			Assert.assertEquals(hp.getmanagerId(), "Manger Id : mngr452393");
+		}
 	}
-	
-	
+
 	@Test
 	public void resetTest() {
 		lp = new loginPage();
 		lp.reset(readconfig.getUserId(), readconfig.getPwd());
 		Assert.assertTrue(true);
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+	}
+
+	public Boolean isAlertPrasent() {
+		try {
+			driver.switchTo().alert();
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
 	}
 
 }
